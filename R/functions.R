81bbb6f94bf74960050fb9e48bb46bab871f79a6
Kristoffer Hellton <hellton@nr.no>
2020-11-24 16:52:57 +0100

    Update functions.R
    Several updates to be inline with the check() function from devtools. Had to change name of functions to be with underline '_' instead of dot '.'

--- R/functions.R
+++ R/functions.R
@@ -6,14 +6,14 @@
 #' @param nw scalar, number of grid points for curve.
 #' @param max.weight scalar, the maximum weight for the curve.
 #' @export
-#' plot.influridge
+#' plot_influridge
 
-plot.influridge <- function(X, y, nw = 100, max.weight = 4, control.list = list(factr = 1e-4)){
+plot_influridge <- function(X, y, nw = 100, max.weight = 4, control.list = list(factr = 1e-4)){
   n <- dim(X)[1]
   weights <- seq(0, max.weight, length.out = nw) # Weight grid
   lambdaMatrix <- matrix(, n, nw)
 
-  startLambda <- optim(
+  startLambda <- stats::optim(
     par = 1, tuning.cv.svd, w = rep(1,n), svd.int = svd(X), y.int = y,
     lower = -Inf, upper = Inf,
     method = "L-BFGS-B", control = control.list
@@ -25,7 +25,7 @@ plot.influridge <- function(X, y, nw = 100, max.weight = 4, control.list = list(
       w[j] <- weights[i] # Weight to give observation j
 
             # find optimal lambda
-      lambdaMatrix[j, i] <- optim(
+      lambdaMatrix[j, i] <- stats::optim(
         par = startLambda, tuning.cv.svd, w = w / sum(w), svd.int = svd(X), y.int = y,
         lower = -Inf, upper = Inf,
         method = "L-BFGS-B", control = control.list
@@ -33,7 +33,7 @@ plot.influridge <- function(X, y, nw = 100, max.weight = 4, control.list = list(
     }
   }
 
-  matplot(weights, t(lambdaMatrix),
+  graphics::matplot(weights, t(lambdaMatrix),
           type = "l", ylim = c(min(lambdaMatrix), max(lambdaMatrix)), xlim = c(0, max(weights) + 0.1),
           xlab = "Weight of observation", ylab = "Tuning parameter", lty = 1, xaxs = "i", yaxs = "i",
           cex.lab = 1.7, mgp = c(2.8, 1, 0), cex.axis = 1.5, col = "grey"
@@ -48,10 +48,10 @@ plot.influridge <- function(X, y, nw = 100, max.weight = 4, control.list = list(
 #' @param svd.int svd object, singular value decomposition of the input matrix.
 #' @param y.int numerical output vector; response variable.
 #' @export
-#' tuning.cv.svd
+#' tuning_cv_svd
 
-tuning.cv.svd <- function(lambda, w, svd.int,y.int) {
+tuning_cv_svd <- function(lambda, w, svd.int,y.int) {
   H <- svd.int$u %*% diag(svd.int$d^2 / (svd.int$d^2 + lambda)) %*% t(svd.int$u)
-  e <- (diag(n) - H) %*% y.int
+  e <- (diag(length(y)) - H) %*% y.int
   return(mean(w * (e / (1 - diag(H)))^2))
 }
